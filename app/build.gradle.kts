1858798814da0ff0a5e65b4f1d7316eaffbbfcb8
ptrdyp <alviany.prameswari@gmail.com>
2023-12-02 22:52:16 +0700

    add scanFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,9 @@ dependencies {
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation ("com.github.dhaval2404:imagepicker:2.1")
+
+    // CameraX Library
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
 }
--- app/src/main/java/com/capstone/basaliproject/ui/scan/TabScanFragment.kt
+++ app/src/main/java/com/capstone/basaliproject/ui/scan/TabScanFragment.kt
@@ -1,27 +1,54 @@
 package com.capstone.basaliproject.ui.scan
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import com.capstone.basaliproject.R
 import com.capstone.basaliproject.databinding.FragmentTabScanBinding
-import com.github.dhaval2404.imagepicker.ImagePicker
+import com.capstone.basaliproject.ui.scan.CameraActivity.Companion.CAMERAX_RESULT
 
 
 class TabScanFragment : Fragment() {
     private var _binding: FragmentTabScanBinding? = null
     private val binding get() = _binding!!
-    lateinit var captureTxt: TextView
     var path: String? = null
-    var uri: Uri? = null
-    private lateinit var captureImage: ImageView
+    private var currentImageUri: Uri? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(requireContext(),
+                    getString(R.string.permission_request_granted), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(),
+                    getString(R.string.permission_request_denied), Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            requireContext(), REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,36 +57,79 @@ class TabScanFragment : Fragment() {
         _binding = FragmentTabScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        captureTxt = binding.titlePlaceholder
-        captureImage = binding.placeholder
-        captureTxt.setOnClickListener {
-            ImagePicker.with(this)
-                .crop(1f, 1f)                   //Crop image(Optional), Check Customization for more option
-                .compress(1024)            //Final image size will be less than 1 MB(Optional)
-                .maxResultSize(
-                    1080,
-                    1080
-                )    //Final image resolution will be less than 1080 x 1080(Optional)
-                .start()
+        binding.captureButton.setOnClickListener {
+            showCustomDialog()
         }
 
         return root
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            //Image Uri will not be null for RESULT_OK
-            val uri: Uri = data?.data!!
+    private fun showCustomDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        val customView = LayoutInflater.from(requireContext()).inflate(R.layout.custom_layout_dialog_scan_option, null)
+        builder.setView(customView)
+        val dialog = builder.create()
 
-            // Use Uri object instead of File to avoid storage permissions
-            captureImage.setImageURI(uri)
-        } else if (resultCode == ImagePicker.RESULT_ERROR) {
-            Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        val cameraButton = customView.findViewById<ImageButton>(R.id.ib_camera)
+        val galleryButton = customView.findViewById<ImageButton>(R.id.ib_gallery)
+        val cancelButton = customView.findViewById<TextView>(R.id.btn_cancel)
+
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        cameraButton.setOnClickListener{
+            val intent = Intent(requireContext(), CameraActivity::class.java)
+            launcherIntentCameraX.launch(intent)
+            dialog.dismiss()
+        }
+
+        galleryButton.setOnClickListener{
+            startGallery()
+            dialog.dismiss()
+        }
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        dialog.show()
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
         } else {
-            Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            Log.d("Photo Picker", "No media selected")
         }
     }
 
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.placeholder.setImageURI(it)
+        }
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERAX_RESULT) {
+            currentImageUri = it.data?.getStringExtra(CameraActivity.EXTRA_CAMERAX_IMAGE)?.toUri()
+            showImage()
+        }
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
 
 }
