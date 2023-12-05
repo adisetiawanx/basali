3d1c535921010cbd8c1c48f9e2561431cf58fc32
shaka pratama ichsan <shakapratama2003@gmail.com>
2023-12-05 20:03:53 +0700

    add recycleview

--- app/src/main/java/com/capstone/basaliproject/ui/learn/LearnFragment.kt
+++ app/src/main/java/com/capstone/basaliproject/ui/learn/LearnFragment.kt
@@ -25,10 +25,7 @@ class LearnFragment : Fragment() {
         _binding = FragmentLearnBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textLearn
-        learnViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
 
     }
