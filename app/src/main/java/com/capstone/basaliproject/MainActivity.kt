458a75d1ae83c8bfe0f1e6c67ee3131200ceaba6
shaka pratama ichsan <shakapratama2003@gmail.com>
2023-12-08 01:14:03 +0700

    using firebase auth, deleting all api

--- app/src/main/java/com/capstone/basaliproject/MainActivity.kt
+++ app/src/main/java/com/capstone/basaliproject/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.capstone.basaliproject
 
-import android.content.Intent
 import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.basaliproject.databinding.ActivityMainBinding
-import com.capstone.basaliproject.ui.Logout.LogOutViewModel
-import com.capstone.basaliproject.ui.ViewModelFactory
-import com.capstone.basaliproject.ui.login.LoginActivity
 import com.capstone.basaliproject.ui.login.LoginViewModel
-import com.capstone.basaliproject.ui.welcome.WelcomeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by viewModels<LoginViewModel> {
-        ViewModelFactory.getInstance(this)
-    }
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/capstone/basaliproject/data/pref/UserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.capstone.basaliproject.data.pref
-
-data class UserModel(
-//    val email: String,
-    val token: String,
-    val isLogin: Boolean = false
-)
